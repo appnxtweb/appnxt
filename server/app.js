40690c03c4a7f50073d7256cefcf51dffca19b35
Abbas Bhanpura wala <abbasbhanpura.dev@gmail.com>
2025-04-20 08:24:22 +0530

    fix: 413 Request Entity Too Large error

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const path = require('path')
-const cors = require('cors')
-const routes = require('./config/allRoutes')
+const path = require("path");
+const cors = require("cors");
+const routes = require("./config/allRoutes");
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static(path.join(__dirname, "assets")));
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow required methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow required headers
+  })
+);
+app.use(routes);
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'assets')))
-app.use(express.urlencoded({ extended : true }))
-app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow required methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Allow required headers
-}));
-app.use(routes)
-
-
-const port = process.env.PORT || 8080
-app.listen(port, ()=>{
-    console.log(`Server is running on : ${port}`)
-})
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on : ${port}`);
+});
